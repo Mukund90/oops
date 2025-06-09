048233389f154bb781f9be1200d22d02cb29ac26
mukundjha <jhamukund986@gmail.com>
2025-06-09 18:54:03 +0530

    upadted

--- oops/encapsulations.cpp
+++ oops/encapsulations.cpp
@@ -4,14 +4,18 @@ class student{
     //default private
     private:
     string studentname;
+    string studentStatus;
     int age;
 
     public:
+       //simple comstructor 
 
-    void displayinfo(){
-        cout<<"studentname : "<<this->studentname<<endl;
-        cout<<"studentage: "<<this->age<<endl;
-    }
+    student(){
+      //This will initialize the value to the class members whenver we created the object 
+       this->studentname = "mukundjha";
+       this->studentStatus = "passed";
+       this->age = 19;
+    };
 
     //we use getters and setters to implement
     public:
@@ -31,16 +35,38 @@ class student{
     int getAge(){
       return this->age;
     }
+   void setStudentStatus(string m){
+     this->studentStatus = m;
+   };
 
+   string getStudentStatus(){
+    return this->studentStatus;
+   }
+
+   void studentInfo(){
+    cout<<endl;
+    cout<<"students_name :"<<this->studentname<<endl;
+    cout<<"studentsStatus :"<<this->studentStatus<<endl;
+    cout<<"studentsAge : "<<this->age<<endl;
+   }
 };
 int main(){
-  //stack based object 
+  //dynamic objects 
   student *t = new student;
-  t->setStudentname("mukundjha"); //set the name to mukundjha
-  cout<<"studentName : "<<t->getStudentname()<<endl; //getting the name 
+  //setting the value to the class members using set method
+  //  t->setStudentname("mukundjha");
+  //  t->setStudentStatus("passed");
+  //  t->setAge(18);
+  cout<<"studentname-> "<<t->getStudentname()<<endl;
+  cout<<"studentstatus->"<<t->getStudentStatus()<<endl;
+  t->studentInfo();//function call
 
-  t->setAge(18);
-  cout<<"age:"<<t->getAge()<<endl;
-  t->displayinfo(); //function calll
+  //stack based objects
+  student s1;
+  s1.studentInfo();//function call
+  s1.setStudentname("amityadav");
+  cout<<s1.getStudentname()<<endl;
+  cout<<"studentName : "<<t->getStudentname()<<endl;
+
    
 }
